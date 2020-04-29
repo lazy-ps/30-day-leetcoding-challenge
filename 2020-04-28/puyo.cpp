ba8d433d9d53331b655b5095c72a153feba45ad2
Kn-Slyr <th.kn.slyr@gmail.com>
2020-04-30 04:10:04 +0900

    add explain

--- 2020-04-28/puyo.cpp
+++ 2020-04-28/puyo.cpp
@@ -1,13 +1,16 @@
+// key point is there is no POP function
 class FirstUnique {
 public:
-    unordered_map<int, int> umap;
-    queue<int> que;
+    unordered_map<int, int> umap;  // save number count
+    queue<int> que;  // added order for unique values
     
     FirstUnique(vector<int>& nums) {
         umap = unordered_map<int, int>();
         que = queue <int>();
         
-        for(auto &v: nums) {
+        // this exactly same as using add function for every element
+        // it means, time complexity is O(N) 
+        for(auto &v: nums) {  
             que.push(v);
             auto iter = umap.find(v);
             if(iter == umap.end()) umap[v] = 1;
@@ -16,9 +19,13 @@ public:
     }
     
     int showFirstUnique() {
+        // this total time complexity is input numbers.
+        // not for one query, all query for O(N)
         while(!que.empty())
         {
             int t = que.front();
+            // if que front is not unique, pop it. 
+            // it never be unique.
             if(umap[t] > 1) que.pop();
             else return t;
         }
@@ -26,9 +33,9 @@ public:
     }
     
     void add(int value) {
-        que.push(value);
+        que.push(value);  // O(1)
         
-        auto iter = umap.find(value);
+        auto iter = umap.find(value);  // O(1) for hash map
         if(iter == umap.end()) umap[value] = 1;
         else iter->second += 1;
     }
